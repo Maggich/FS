52ee012f65c829d10867e2c5f45bdeb2d7514003
Maggich <mvahaevich@mail.ru>
2026-09-03 14:24:28 +0500

    rad

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Header/header"
+import CourseCard from "./Components/CourseCard/coursecard"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <CourseCard />
+    </>
+  )
+}
+
+export default App
